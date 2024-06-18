d6e1b49fb9bc0c8189d5eaf01e1bc13cf778e2a3
alexandruARV <alex.alex12345100@yahoo.com>
2024-06-18 21:45:52 +0300

    v0.5

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,10 +1,13 @@
-import { Link } from "react-router-dom";
-import SearchOrder from "../features/order/SearchOrder";
+import { Link } from 'react-router-dom';
+import SearchOrder from '../features/order/SearchOrder';
 
 function Header() {
   return (
-    <header>
-      <Link to="/">Fast React pizza Co.</Link>
+    <header className="bg-yellow-500 uppercase">
+      {/* <Link to="/" className="tracking-[.5rem]"> */}
+      <Link to="/" className="tracking-widest">
+        Fast React pizza Co.
+      </Link>
       <SearchOrder />
       <p>Jhonas</p>
     </header>
