3e3ca2ac17b1e3dcd8e3fc37e2e8136b82e6a9ab
alexandruARV <alex.alex12345100@gmail.com>
2024-06-25 09:53:32 +0300

    v1

--- src/features/order/Order.jsx
+++ src/features/order/Order.jsx
@@ -25,7 +25,7 @@ function Order() {
   const deliveryIn = calcMinutesLeft(estimatedDelivery);
 
   return (
-    <div className="space-y-8 px-6 py-5">
+    <div className="space-y-8 px-6 py-6">
       <div className="flex flex-wrap items-center justify-between gap-2">
         <h2 className="text-xl font-semibold">Order #{id} Status</h2>
 
